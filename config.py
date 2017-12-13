9bafe84db8cc084bfe99f811af11509e4466b366
Joy Gao <joyg@wepay.com>
2017-12-12 17:02:13 -0800

    Use same secret key as airflow core

--- config.py
+++ config.py
@@ -16,9 +16,6 @@ import os
 from flask_appbuilder.security.manager import AUTH_OID, AUTH_REMOTE_USER, AUTH_DB, AUTH_LDAP, AUTH_OAUTH
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# Your App secret key
-SECRET_KEY = '\2\1thisismyscretkey\1\2\e\y\y\h'
-
 # The SQLAlchemy connection string.
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'airflow.db')
 #SQLALCHEMY_DATABASE_URI = 'mysql://myapp@localhost/myapp'
